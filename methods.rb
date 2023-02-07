a93c3451e4d1d5edf008d0aea9c61c1f24a31d32
Joseph <wamiti8711@gmail.com>
2023-02-07 11:02:08 -0500

    methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+
+
+def greet_Programmer
+    puts "Hello, programmer!"
+end
+
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+greet(Jimmy)
+
+def greet_with_default name
+    puts "Hello, #{name}!"
+end
+
+greet_with_default(Naureen)
+
+def add num1, num2
+    puts num1 + num2
+end
+
+add(1,2)
+
+def halve num3
+    puts num2 / 2
+end
+
+halve(2)
